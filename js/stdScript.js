61d6ab52c2fdfb60dc8eee6a3d7c59824fee6bdb
Ninad <89674431+MasterNinad@users.noreply.github.com>
2022-12-07 22:06:04 +0530

    Create stdScript.js

---
+++ js/stdScript.js
@@ -0,0 +1,42 @@
+const validChars = ["+", "-", "×", "÷", "%", ".", "/", "*"];
+
+window.onload = function () {
+    let input = document.getElementById("mainBody");
+    input.addEventListener("keypress", (e) => {
+        if (e.keyCode == 13) {
+            calculate();
+        }
+    }, false)
+}
+
+function ansKey(e) {
+    let key = e.key;
+    if (isNaN(key)) {
+        if (!validChars.includes(key)) {
+            e.preventDefault();
+        }
+    }
+}
+
+function appendText(text) {
+    let ans = document.getElementById("answer");
+    ans.value += text.value;
+}
+
+function clearText() {
+    document.getElementById("answer").value = " ";
+}
+
+function lastClearText() {
+    let text = document.getElementById("answer").value;
+    let result = text.slice(0, -1);
+    document.getElementById("answer").value = result;
+}
+
+function calculate() {
+    let input = document.getElementById("answer").value;
+    input = input.replace("÷", "/");
+    input = input.replace("×", "*")
+    let result = Function("return " + input)();
+    document.getElementById("answer").value = result;
+}
