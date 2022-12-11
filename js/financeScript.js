f21835c1b4fbc5adad563f0199964437b8451a89
Ninad <kninad2008@gmail.com>
2022-12-11 16:37:22 +0530

    checked grammer

--- js/financeScript.js
+++ js/financeScript.js
@@ -2,29 +2,29 @@ var principal = document.getElementById("principalText");
 var years = document.getElementById("yearsText");
 var rate = document.getElementById("rateText");
 var amount = document.getElementById("amountText");
-var intrest = document.getElementById("intrestText");
+var interest = document.getElementById("interestText");
 
 function principalFunc() {
-    if (intrest.value == "" || rate.value == "" || years.value == "") {
+    if (interest.value == "" || rate.value == "" || years.value == "") {
         console.log("error");
     }
-    else if (intrest.value != "" && amount.value != "") {
-        var ans = amount.value - intrest.value;
+    else if (interest.value != "" && amount.value != "") {
+        var ans = amount.value - interest.value;
         principal.value = ans;
     }
     else {
         var percentRate = rate.value / 100;
-        var ans = intrest.value / (percentRate * years.value);
+        var ans = interest.value / (percentRate * years.value);
         principal.value = ans;
     }
 }
 
 function rateFunc() {
-    if (intrest.value == "" || principal.value == "" || years.value == "") {
+    if (interest.value == "" || principal.value == "" || years.value == "") {
         console.log("error");
     }
     else {
-        var ans = intrest.value / (principal.value * years.value);
+        var ans = interest.value / (principal.value * years.value);
         rate.value = ans;
         document.getElementById("ratePercentText").innerHTML = ans * 100 + "%";
     }
