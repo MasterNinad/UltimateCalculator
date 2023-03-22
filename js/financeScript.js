84f3a7c7527a66d97467724dc0876244b300f4aa
Ninad <kninad2008@gmail.com>
2023-03-22 11:05:08 +0530

    added amount and intrest functions

--- js/financeScript.js
+++ js/financeScript.js
@@ -31,11 +31,48 @@ function rateFunc() {
 }
 
 function yearsFunc() {
-    if (interest.value == "" || principal.value == "" || rate.value == "") {
-        console.log("error");
+    if (interest.value == "") {
+        alert("Please enter Intrest to find Years!");
+    }
+    else if (principal.value == "") {
+        alert("Please enter Principal to find Years!");
+    }
+    else if (rate.value == "") {
+        alert("Please enter Rate to find Years!");
     }
     else {
         let ans = (100 * interest.value) / (principal.value * rate.value);
         years.value = ans;
+    }
+}
+
+function amountFunc() {
+    if (intrest.value == "") {
+        alert("Please enter Intrest to find Amount!");
+    }
+    else if (principal.value == "") {
+        alert("Please enter Principal to find Amount!");
+    }
+    else {
+        amount.value = intrest.value + principal.value;
+    }
+}
+
+function intrestFunc() {
+    if (amount.value != "" && principal.value != "") {
+        intrest.value = amount.value - principal.value;
+    }
+    else if (principal.value == "") {
+        alert("Please enter Principal to find Intrest!");
+    }
+    else if (rate.value == "") {
+        alert("Please enter Rate to find Intrest!");
+    }
+    else if (years.value == "") {
+        alert("Please enter Years to find Intrest!");
+    }
+    else {
+        let ans = (principal.value * rate.value * years.value) / 100;
+        intrest.value = ans;
     }
 }
