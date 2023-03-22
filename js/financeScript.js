b4133b7d3bb7182fe4d5efc4d7daa771a11534ef
Ninad <kninad2008@gmail.com>
2023-03-22 09:25:22 +0530

    added years function in finance

--- js/financeScript.js
+++ js/financeScript.js
@@ -1,20 +1,20 @@
-var principal = document.getElementById("principalText");
-var years = document.getElementById("yearsText");
-var rate = document.getElementById("rateText");
-var amount = document.getElementById("amountText");
-var interest = document.getElementById("interestText");
+let principal = document.getElementById("principalText");
+let years = document.getElementById("yearsText");
+let rate = document.getElementById("rateText");
+let amount = document.getElementById("amountText");
+let interest = document.getElementById("interestText");
 
 function principalFunc() {
     if (interest.value == "" || rate.value == "" || years.value == "") {
         console.log("error");
     }
     else if (interest.value != "" && amount.value != "") {
-        var ans = amount.value - interest.value;
+        let ans = amount.value - interest.value;
         principal.value = ans;
     }
     else {
-        var percentRate = rate.value / 100;
-        var ans = interest.value / (percentRate * years.value);
+        let percentRate = rate.value / 100;
+        let ans = interest.value / (percentRate * years.value);
         principal.value = ans;
     }
 }
@@ -24,8 +24,18 @@ function rateFunc() {
         console.log("error");
     }
     else {
-        var ans = interest.value / (principal.value * years.value);
+        let ans = interest.value / (principal.value * years.value);
         rate.value = ans;
         document.getElementById("ratePercentText").innerHTML = ans * 100 + "%";
+    }
+}
+
+function yearsFunc() {
+    if (interest.value == "" || principal.value == "" || rate.value == "") {
+        console.log("error");
+    }
+    else {
+        let ans = (100 * interest.value) / (principal.value * rate.value);
+        years.value = ans;
     }
 }
