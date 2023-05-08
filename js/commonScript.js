becd82dcffb82346b5c36f5b4f0ab942db311f23
Ninad <89674431+MasterNinad@users.noreply.github.com>
2023-05-08 18:37:59 +0530

    added 2nd id option foe clear text

--- js/commonScript.js
+++ js/commonScript.js
@@ -1,3 +1,6 @@
-function clearText(id) {
+function clearText(id, id2) {
     document.getElementById(id).value = "";
+    if (id2 != undefined) {
+        document.getElementById(id2).value = "";
+    }
 };
