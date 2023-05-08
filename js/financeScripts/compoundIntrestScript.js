58e80c3baa0a93357a7a7a06635ac83a949b81da
Ninad <89674431+MasterNinad@users.noreply.github.com>
2023-05-08 19:07:21 +0530

    updated compound script

---
+++ js/financeScripts/compoundIntrestScript.js
@@ -0,0 +1,5 @@
+let principal = document.getElementById("CIprincipalText");
+let years = document.getElementById("CIyearsText");
+let rate = document.getElementById("CIrateText");
+let amount = document.getElementById("CIamountText");
+let interest = document.getElementById("CIinterestText");
