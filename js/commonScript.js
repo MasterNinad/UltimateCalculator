5fba5bc15a59dd3a26fa7154aea4970fb2c63200
Ninad <89674431+MasterNinad@users.noreply.github.com>
2023-05-08 18:47:28 +0530

    changed value to innerText

--- js/commonScript.js
+++ js/commonScript.js
@@ -1,6 +1,6 @@
 function clearText(id, id2) {
     document.getElementById(id).value = "";
     if (id2 != undefined) {
-        document.getElementById(id2).value = "";
+        document.getElementById(id2).innerText = "";
     }
 };
